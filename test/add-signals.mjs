7810c056264ccaf9fe81fecf5105f9bfda968472
Alan Layng <alan.layng@gmail.com>
2024-06-06 11:36:34 +0100

    move to node test runner

--- test/add-signals.mjs
+++ test/add-signals.mjs
@@ -1,36 +1,36 @@
-import { test } from 'uvu'
-import * as assert from 'uvu/assert'
+import { suite, test } from 'node:test'
+import assert from 'node:assert/strict'
 
 import addSignals from '../src/add-signals.mjs'
 import { Signal } from '@preact/signals-core'
 
-test('basic signals', () => {
-  const obj = {}
-  const ret = addSignals(obj, {
-    foo: 'bar'
+suite('addSignals', () => {
+  test('basic signals', () => {
+    const obj = {}
+    const ret = addSignals(obj, {
+      foo: 'bar'
+    })
+
+    assert.equal(ret, obj)
+    assert.ok(obj.$foo instanceof Signal)
+    assert.equal(obj.foo, 'bar')
+
+    obj.foo = 'baz'
+    assert.equal(obj.$foo.value, 'baz')
   })
 
-  assert.is(ret, obj)
-  assert.instance(obj.$foo, Signal)
-  assert.equal(obj.foo, 'bar')
+  test('computed', () => {
+    const obj = {}
+    addSignals(obj, {
+      foo: 17,
+      bar: () => obj.foo + 1
+    })
 
-  obj.foo = 'baz'
-  assert.equal(obj.$foo.value, 'baz')
-})
+    assert.ok(obj.$foo instanceof Signal)
+    assert.ok(obj.$bar instanceof Signal)
+    assert.equal(obj.foo, 17)
+    assert.equal(obj.bar, 18)
 
-test('computed', () => {
-  const obj = {}
-  addSignals(obj, {
-    foo: 17,
-    bar: () => obj.foo + 1
+    assert.throws(() => (obj.bar = 19))
   })
-
-  assert.instance(obj.$foo, Signal)
-  assert.instance(obj.$bar, Signal)
-  assert.equal(obj.foo, 17)
-  assert.equal(obj.bar, 18)
-
-  assert.throws(() => (obj.bar = 19))
 })
-
-test.run()
--- test/until.mjs
+++ test/until.mjs
@@ -1,45 +1,46 @@
-import { test } from 'uvu'
-import * as assert from 'uvu/assert'
+import { suite, test } from 'node:test'
+import assert from 'node:assert/strict'
+
 import { signal } from '@preact/signals-core'
 import sleep from 'pixutil/sleep'
 import isResolved from 'pixutil/is-resolved'
 
 import until from '../src/until.mjs'
 
-test('basic wait', async () => {
-  const $s = signal(1)
-  const test = () => $s.value === 2
-  const p = until(test)
+suite('until', () => {
+  test('basic wait', async () => {
+    const $s = signal(1)
+    const test = () => $s.value === 2
+    const p = until(test)
 
-  assert.instance(p, Promise)
-  assert.is(await isResolved(p), false)
+    assert.ok(p instanceof Promise)
+    assert.equal(await isResolved(p), false)
 
-  $s.value = 2
-  assert.is(await isResolved(p), true)
-  assert.is(await p, true)
+    $s.value = 2
+    assert.equal(await isResolved(p), true)
+    assert.equal(await p, true)
+  })
+
+  test('already true', async () => {
+    const $s = signal(1)
+    const test = () => $s.value === 1
+    const p = until(test)
+
+    assert.ok(p instanceof Promise)
+    assert.equal(await isResolved(p), true)
+    assert.equal(await p, true)
+  })
+
+  test('timeout', async () => {
+    const $s = signal(1)
+    const test = () => $s.value === 2
+    const p = until(test, 50)
+
+    assert.ok(p instanceof Promise)
+    assert.equal(await isResolved(p), false)
+
+    await sleep(75)
+    assert.equal(await isResolved(p), true)
+    assert.equal(await p, false)
+  })
 })
-
-test('already true', async () => {
-  const $s = signal(1)
-  const test = () => $s.value === 1
-  const p = until(test)
-
-  assert.instance(p, Promise)
-  assert.is(await isResolved(p), true)
-  assert.is(await p, true)
-})
-
-test('timeout', async () => {
-  const $s = signal(1)
-  const test = () => $s.value === 2
-  const p = until(test, 100)
-
-  assert.instance(p, Promise)
-  assert.is(await isResolved(p), false)
-
-  await sleep(150)
-  assert.is(await isResolved(p), true)
-  assert.is(await p, false)
-})
-
-test.run()
